62811baca9b3bd11138e0135981b10bbf8497ecd
Hyeong-Yun <guddbs5545@inu.ac.kr>
2021-08-26 21:35:47 +0900

    hyeongyun

--- src/index.js
+++ src/index.js
@@ -2,16 +2,30 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import {BrowserRouter, Route} from 'react-router-dom'
+
+function Home(){
+  return (
+    <div>
+      <h2>Home</h2>
+      Home...
+    </div>
+  )
+}
+function Topics(){
+  return (
+    <div>
+      <h2>Topics</h2>
+      Home...
+    </div>
+  )
+}
+
 
 ReactDOM.render(
-  <React.StrictMode>
+  <BrowserRouter>
     <App />
-  </React.StrictMode>,
+  </BrowserRouter>,
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
