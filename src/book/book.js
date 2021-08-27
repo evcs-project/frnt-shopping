af845337638193ff698f83ba952742279f6c70b3
Doyeon04 <rlaehdus0411@naver.com>
2021-08-27 17:29:32 +0900

    ㅇㄹㅇ

--- src/book/book.js
+++ src/book/book.js
@@ -1,20 +1,20 @@
-class Book{
- constructor(key){
-   this.key = key;
-   this.getRequestOption = {
-    method: 'GET',
-    redirect: 'follow'
-   }
- } 
+class Book {
+  constructor(key) {
+    this.key = key;
+    this.getRequestOption = {
+      method: "GET",
+      redirect: "follow",
+    };
+  }
 
- async search(query){
-   const response = await fetch(
-     `http://52.79.149.9:8080/v2/api-docs/api/book/search?bookNm=${query}`, 
+  async search(query) {
+    const response = await fetch(
+      `http://52.79.149.9:8080/v2/api-docs/api/book/search?bookNm=${query}`,
       this.getRequestOption
-   );
-   const result_1 = await response.json();
-    console.log(result_1)
-    
-  //  return result_1.items.map(item => ({ ...item, id: item.id.videoId }));
- }
+    );
+    const result_1 = await response.json();
+    console.log(result_1);
+
+    //  return result_1.items.map(item => ({ ...item, id: item.id.videoId }));
+  }
 }
