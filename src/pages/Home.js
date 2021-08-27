f36c3140521bd45333bd99254f8935b770b3fe0a
Doyeon04 <rlaehdus0411@naver.com>
2021-08-27 16:05:36 +0900

    kdy: select 값 가져오기

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,33 +1,63 @@
-import React from "react";
+import React, { Component } from "react";
+// import { useForm } from "react-hook-form";
 import "../css/Home.css";
 
-const Home = () => {
-  return (
-    <header>
-      <ul className="top-bar__menus">
-        <li className="top-bar__menu"><a href = "/Login">로그인</a></li>
-        <li className="top-bar__menu">회원가입</li>
-        <li className="top-bar__menu">마이페이지</li>
-      </ul>
-      
-      <div className="top-bar__searchBox">
-        <i className="fas fa-book"></i>
-        <div className="top-bar__searchBox__search">
-          <select name="bookType" id="bookType">
-            <option value="name">제목</option>
-            <option value="name">저자</option>
-            <option value="name">출판사</option>
-            <option value="name">제목</option>
-            <option value="name">제목</option>
-          </select>
-          <input type="text" id="search"></input>
-          <button className="searchBtn">
-            <i className="fas fa-search"></i>
-          </button>
+class Home extends Component {
+  selectRef = React.createRef();
+
+  componentDidMount() {
+    console.log(this.selectRef.current.value);
+  }
+
+  // componentDidUpdate() {
+  //   console.log(this.selectRef.current.value);
+  // }
+
+  changeSelect = (e) => {
+    let select = "bookNm";
+    const selectedValue = document.getElementById("bookType");
+    select = selectedValue.options[selectedValue.selectedIndex].value;
+    console.log(select);
+  };
+  render() {
+    return (
+      <header>
+        <ul className="top-bar__menus">
+          <li className="top-bar__menu">
+            <a href="/Login">로그인</a>
+          </li>
+          <li className="top-bar__menu">회원가입</li>
+          <li className="top-bar__menu">마이페이지</li>
+        </ul>
+
+        <div className="top-bar__searchBox">
+          <i className="fas fa-book"></i>
+          <div className="top-bar__searchBox__search">
+            <select
+              ref={this.selectRef}
+              name="bookType"
+              id="bookType"
+              onChange={this.changeSelect}
+            >
+              <option value="bookNm">제목</option>
+              <option value="publisher">출판사</option>
+              <option value="writer">저자</option>
+            </select>
+            <input type="text" id="search"></input>
+            <button className="searchBtn">
+              <i className="fas fa-search"></i>
+            </button>
+          </div>
         </div>
-      </div>
-    </header>
-  );
-};
+      </header>
+    );
+
+    const select = document.getElementById("bookType");
+
+    const selectedValue = select.options[select.selectedIndex].value;
+
+    console.log(selectedValue);
+  }
+}
 
 export default Home;
