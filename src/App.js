9c2a74703b31bccada0763fcdcb79dd0dcead9eb
Hyeong-Yun <hyeongyungim7@gmail.com>
2021-08-27 13:56:19 +0900

    login change

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import { BrowserRouter, Route } from "react-router-dom";
 import Home from './pages/Home'
 import Login from './pages/Login'
-
+import Home from './pages/Home'
 
 function App() {
+
   return (
     <div className="App">
-      <Route path="/" component = {Home} exact = {true}></Route>
+      <Route path="/" exact = {true}><Home /></Route>
       <Route path="/login" component = {Login} ></Route>
     </div>
   );
