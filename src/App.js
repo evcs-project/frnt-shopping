3e1b174b52733be96a677f5ecee2284ee048a1e1
Doyeon04 <rlaehdus0411@naver.com>
2021-08-28 20:21:23 +0900

    페이징 처리

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 import React, { useState } from "react";
-import _ from 'lodash'
+import _ from "lodash";
 
 function paginate(items, pageNumber, pageSize) {
   const startIndex = (pageNumber - 1) * pageSize; // 자를 배열의 시작점
@@ -14,7 +14,13 @@ function paginate(items, pageNumber, pageSize) {
     .value(); // lodash wrapper 객체를 regular 배열로 변환
 }
 
-function App(){
+const queryLS = localStorage.getItem("query");
+const selectLS = localStorage.getItem("select");
+
+console.log("query:", queryLS);
+console.log("select:", selectLS);
+
+function App() {
   const [data, setData] = useState([]);
 
   const search = (select, query) => {
@@ -27,7 +33,7 @@ function App(){
 
     if (select !== "" && query !== "") {
       fetch(
-        `http://13.125.22.103:8080/api/book/search?size=25&page=0&${select}=${query}`,
+        `http://13.125.22.103:8080/api/book/search?size=5&page=0&${select}=${query}`,
         requestOptions
       )
         .then((response) => response.json())
@@ -48,42 +54,74 @@ function App(){
           }));
         })
         .then((items) => {
-          setData(items)
+          setData(items);
         })
         .catch((error) => console.log("error", error));
     }
   };
 
-   const pageSize = 2
-   const itemsCount=  10
-  const currentPage=  1
+  const pageSize = 3;
+  const itemsCount = 10;
+  const currentPage = 1;
 
-  const pagedBooks = paginate(data, currentPage, pageSize)
+  const pagedBooks = paginate(data, currentPage, pageSize);
 
   const handlePageChange = (page) => {
-    setData({ ...data, currentPage: page });
-  }
+    // setData({ ...data, currentPage: page });
+    const requestOptions = {
+      method: "GET",
+      redirect: "follow",
+    };
+    fetch(
+      `http://13.125.22.103:8080/api/book/search?size=5&page=${page}&${selectLS}=${queryLS}`,
+      requestOptions
+    )
+      .then((response) => response.json())
+      .then((result) => {
+        console.log(result);
+        if (result.content.length === 0) {
+          alert("찾으시는 책이 없습니다.");
+        }
 
-   const handleValueSet = (select, inputValue) => {
-    search(select, inputValue);
+        return result.content.map((item) => ({
+          id: item.bookId,
+          title: item.bookNm,
+          category: item.categoryName,
+          price: item.price,
+          publisher: item.publisher,
+          img: item.thumbnailUrl,
+          writer: item.writer,
+        }));
+      })
+      .then((items) => {
+        setData(items);
+      })
+      .catch((error) => console.log("error", error));
   };
-  
-    return (
-      <div className="App">
-        <Route path="/" exact={true}>
-          <Home
-            books={pagedBooks}
-            onChange={handleValueSet}
-            onPageChange={handlePageChange}
-            pageSize= {pageSize} 
-            itemsCount={itemsCount} 
-            currentPage={currentPage} 
-          />
-        </Route>
-        <Route path="/login" component={Login}></Route>
-        <Route path="/Signup" component={Signup}></Route>
-      </div>
-    );
+
+  const handleValueSet = (select, inputValue) => {
+    search(select, inputValue);
+
+    localStorage.setItem("query", inputValue);
+    localStorage.setItem("select", select);
+  };
+
+  return (
+    <div className="App">
+      <Route path="/" exact={true}>
+        <Home
+          books={pagedBooks}
+          onChange={handleValueSet}
+          onPageChange={handlePageChange}
+          pageSize={pageSize}
+          itemsCount={itemsCount}
+          currentPage={currentPage}
+        />
+      </Route>
+      <Route path="/login" component={Login}></Route>
+      <Route path="/Signup" component={Signup}></Route>
+    </div>
+  );
 }
 
 export default App;
