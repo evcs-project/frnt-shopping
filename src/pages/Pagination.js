836e189a179685506eabec5ab7afb99a31a8f760
Doyeon04 <rlaehdus0411@naver.com>
2021-08-29 21:42:15 +0900

    버튼 10개씩 자르기

--- src/pages/Pagination.js
+++ src/pages/Pagination.js
@@ -1,5 +1,5 @@
 import React from "react";
-import _ from "lodash";
+import _, { constant, lte } from "lodash";
 import "../css/page.css";
 
 const Pagination = ({
@@ -12,13 +12,48 @@ const Pagination = ({
   const pageCount = totalPages;
 
   // if (pageCount === 1) return null;
-  const pages = _.range(1, pageCount + 1);
+  let pages = _.range(1, pageCount + 1);
 
-  console.log("pagination 함수 실행됨");
+  let realPagesArr = pages;
+  console.log(realPagesArr);
+
+  if (pages.length > 10) {
+    console.log("page개수 10보다 큼");
+    pages = pages.slice(0, 10);
+  }
+
+  // let currentShowingPage = pages.length;
+
+  // console.log(currentShowingPage);
+
+  // let currentShowingPageArr = [];
+
+  // function showPage(currentShowingPage) {
+  //   for (let i = 1; i <= 10; i++) {
+  //     currentShowingPageArr.push(currentShowingPage + i);
+  //   }
+  //   console.log(currentShowingPageArr);
+  // }
+  let start = 10;
+  let end = 20;
+
+  // 34
+
+  let realPagesArr2 = new Array();
+
+  function showPage(pageStart, pageEnd) {
+    console.log(pageStart, pageEnd, "자름");
+    realPagesArr2 = realPagesArr.slice(pageStart, pageEnd);
+
+    start += 10;
+    end += 10;
+  }
+  console.log(realPagesArr2);
+
   return (
     <nav>
       <ul className="pagination">
-        {pages.map((page) => (
+        {realPagesArr2.map((page) => (
           <li
             key={page}
             className={page === currentPage ? "page-item active" : "page-item"}
@@ -31,6 +66,12 @@ const Pagination = ({
           </li>
         ))}
       </ul>
+      <button
+        onClick={() => showPage(start, end)}
+        className={pages.length === 10 ? "pageBtn active" : "pageBtn"}
+      >
+        버튼
+      </button>
     </nav>
   );
 };
